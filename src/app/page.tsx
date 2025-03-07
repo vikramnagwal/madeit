3990dd9e294e549ccfce974668c48c1a14d16983
Vikram Singh <nagwalanshu@gmail.com>
2025-03-07 22:49:28 +0530

    refactor: update project structure and add new icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,63 @@
-"use client"
+'use client';
 
+import { Mobile } from "@/packages/components/mobile";
+import { NotificationCard } from "@/packages/components/notification-card";
 import { Tab } from "@/packages/ui/tab";
 
+
 export default function Home() {
-	
-	return (
-		<div>
-			<h1>hello world</h1>
-			<Tab />
-		</div>
-	);
+  
+  return (
+    <div>
+      <Tab
+        variant="ghost"
+        options={[
+          {
+            id: "iphone",
+            label: "Iphone",
+            children: (
+              <Mobile
+                variant="iphone"
+                notificationNumber={2}
+                options={{
+                  networkCarrier: "Airtel",
+                  wallpaper:
+                    "https://images.unsplash.com/photo-1541348263662-e068662d82af?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGNhcnN8ZW58MHx8MHx8fDA%3D",
+                }}
+                children={
+                  <NotificationCard
+                    from="Paypal"
+                    description="you have recieved 100$ from"
+                    sender="user@123.pypl.com"
+                    time={new Date()}
+                    timeAgo={2}
+                  />
+                }
+              />
+            ),
+          },
+          {
+            id: "android",
+            label: "Android",
+            children: (
+              <Mobile
+                variant="android"
+                options={{
+                  networkCarrier: "AT&T",
+                  wallpaper:
+                    "https://images.unsplash.com/photo-1617814076367-b759c7d7e738?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTB8fGNhcnN8ZW58MHx8MHx8fDA%3D",
+                }}
+                children={
+                  <NotificationCard
+                    description="you have recieved 100$ from"
+                    sender="user@123.pypl.com"
+                  />
+                }
+              />
+            ),
+          },
+        ]}
+      />
+    </div>
+  );
 }
--- src/packages/components/mobile.tsx
+++ src/packages/components/mobile.tsx
@@ -1,45 +1,90 @@
+'use client';
+
 import { cva, VariantProps } from "class-variance-authority";
-import { PropsWithChildren, createContext } from "react";
+import { PropsWithChildren, createContext, useEffect, useState } from "react";
 import { cn } from "../utils/cn";
+import moment from 'moment'; 
 
 const mobileVariants = cva(
-	"flex flex-col relative w-[300px] h-[600px] bg-cover mx-auto shadow-md overflow-hidden",
-	{
-		variants: {
-			variant: {
-				iphone: "rounded-[10.5mm] ring ring-8 ring-slate-800",
-				android: "rounded-[5.5mm] ring ring-6 ring-slate-300",
-			},
-		},
-		defaultVariants: {
-			variant: "iphone",
-		},
-	},
+  "flex flex-col relative w-[300px] h-[600px] bg-cover mx-auto shadow-md overflow-hidden",
+  {
+    variants: {
+      variant: {
+        iphone: "rounded-[10.5mm] ring ring-12 ring-slate-800",
+        android: "rounded-[5.5mm] ring ring-8 ring-slate-300",
+      },
+    },
+    defaultVariants: {
+      variant: "iphone",
+    },
+  }
 );
 
 type MobileProps = PropsWithChildren<{
-	variant: "iphone" | "android";
-	className?: string;
+  variant: "iphone" | "android";
+  notificationNumber?: number;
+  options: MobileOptionsProps;
+  className?: string;
+  children: React.ReactNode;
 }> &
-	VariantProps<typeof mobileVariants>;
+  VariantProps<typeof mobileVariants>;
+
+type MobileOptionsProps = {
+  networkCarrier?:
+    | "Airtel"
+    | "Vi"
+    | "Jio"
+    | "BSNL"
+    | "Sprint"
+    | "T-Mobile"
+    | "Verizon"
+    | "AT&T"
+    | "TFW"
+    | "MetroPCS";
+    wallpaper: string;
+};
 
 export const MobileTypeContext = createContext<Pick<MobileProps, "variant">>({
-	variant: "iphone",
+  variant: "iphone",
 });
 
-export function Mobile({
-	variant = "iphone",
-	className,
-	children,
-}: MobileProps) {
-	return (
-		<div className={cn(mobileVariants({ variant }), className)}>
-			<img
-				src="https://images.unsplash.com/photo-1740927726070-26542d1a9b04?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxN3x8fGVufDB8fHx8fA%3D%3D"
-				alt="image"
-				className="w-full h-full object-cover"
-			/>
-			<MobileTypeContext value={{ variant }}>{children}</MobileTypeContext>
-		</div>
-	);
+
+export function Mobile({ variant, notificationNumber = 1,options ,className, children }: MobileProps) {
+
+  const notifications = Array.from({ length: notificationNumber}, (_, i) => i);
+  const hours = moment().format('h:mm');
+
+  const [time, setTime] = useState(hours);
+
+  return (
+    <div className={cn(mobileVariants({ variant }), "relative", className)}>
+      <img
+        src={options.wallpaper}
+        alt="wallpaper"
+        className="absolute inset-0 w-full h-full object-cover -z-10"
+      />
+      <span className="absolute top-2 left-6 opacity-80 text-xs z-20">{options.networkCarrier}</span>
+      <div className="mx-auto text-white mt-[40px]">
+        <span className="text-[50px] leading-loose slashed-zero font-poppins font-[600]">
+          {time}
+        </span>
+      </div>
+      {variant === "iphone" ? (
+        <div className="flex justify-end items-center px-2 absolute top-2 left-1/2 -translate-x-1/2 bg-black z-20 w-24 h-8 rounded-full">
+          <div className="w-4 h-4 rounded-full bg-white/20" />
+        </div>
+      ) : (
+        <div>
+          <div className="w-4 h-4 rounded-full bg-black/90 z-20 absolute top-2 left-1/2 -translate-x-1/2" />
+        </div>
+      )}
+      <div className="relative z-10 flex flex-col mt-14">
+        <MobileTypeContext.Provider value={{ variant }}>
+          {notifications.map((_, index) => (
+            <div key={index}>{children}</div>
+          ))}
+        </MobileTypeContext.Provider>
+      </div>
+    </div>
+  );
 }
